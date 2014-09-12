2f0001dd5ce5f55273eda9c900c4f2d32c9848b0
Christopher Petrilli <petrilli@amber.org>
2014-09-11 20:12:06 -0700

    Initial RESTful server. This is just a placeholder, and for debugging purposes.

--- pkpserver/pkpserver.py
+++ pkpserver/pkpserver.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+from flask import Flask
+from flask.ext import restful
+
+
+app = Flask(__name__)
+api = restful.Api(app)
+
+# If we're run as a script, start a debug server
+if __name__ == '__main__':
+    app.run(debug=True)
