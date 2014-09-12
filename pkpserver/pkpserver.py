f76edaec121f67f5625579f73867fb864d04509b
Christopher Petrilli <petrilli@amber.org>
2014-09-11 20:22:06 -0700

    Placeholder Pin Validation error resource.

--- pkpserver/pkpserver.py
+++ pkpserver/pkpserver.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask.ext import restful
-
+from .resources.pin_validation_error import PinValidationError
 
 app = Flask(__name__)
 api = restful.Api(app)
+
+# Resources
+api.add_resource(PinValidationError, '/')
 
 # If we're run as a script, start a debug server
 if __name__ == '__main__':
---
+++ pkpserver/resources/pin_validation_error.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from flask.ext import restful
+
+
+class PinValidationError(restful.Resource):
+    """Represents a single instance of a Pin Validation error from a user
+    agent (UA) when a Known Pinned Host has set the report-uri directive."""
+    def post(self):
+        pass
